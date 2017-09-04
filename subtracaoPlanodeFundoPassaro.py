cf89519063e7c37b100962272d4d82a0bc20bb33
Patrícia Duarte <patricia.duarte.dds@gmail.com>
2017-09-04 20:50:50 -0300

    Update subtracaoPlanodeFundoPassaro.py

--- subtracaoPlanodeFundoPassaro.py
+++ subtracaoPlanodeFundoPassaro.py
@@ -2,9 +2,9 @@ import cv2
 import numpy as np
 
 
-src = cv2.imread('bird.jpg',0);
-th2 = cv2.adaptiveThreshold(src,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,15,5)
-newImage = th2
-newImage = cv2.medianBlur(newImage,5)
-cv2.imshow("Final Image", newImage)
+imagem = cv2.imread('bird.jpg',0);
+threshold = cv2.adaptiveThreshold(imagem,255,cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,15,5)
+novaImagem = threshold
+novaImagem = cv2.medianBlur(novaImagem,5)
+cv2.imshow("Imagem final:", novaImagem)
 cv2.waitKey(0)
