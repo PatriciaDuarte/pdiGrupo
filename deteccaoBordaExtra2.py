8baff834cf76c905e74aa91e8778999bc83053ef
Patrícia Duarte <patricia.duarte.dds@gmail.com>
2017-09-04 13:34:57 -0300

    Update deteccaoBordaExtra2.py

--- deteccaoBordaExtra2.py
+++ deteccaoBordaExtra2.py
@@ -1,11 +1,10 @@
-import cv2
 import numpy as np
-from matplotlib import pyplot as plt
-
-img = cv2.imread('alanturing.jpg',0)
-edges = cv2.Canny(img,100,200)
-plt.subplot(121),plt.imshow(img,cmap = 'gray')
-plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-plt.subplot(122),plt.imshow(edges,cmap = 'gray')
-plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
-plt.show()
+import cv2
+img = cv2.imread('alanturing.jpg')
+img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+suave = cv2.GaussianBlur(img, (7, 7), 0)
+canny1 = cv2.Canny(suave, 20, 120)
+canny2 = cv2.Canny(suave, 70, 200)
+resultado = np.vstack([np.hstack([img,    suave ]),np.hstack([canny1, canny2])]) 
+cv2.imshow("Detector de Bordas Canny", resultado)
+cv2.waitKey(0)
