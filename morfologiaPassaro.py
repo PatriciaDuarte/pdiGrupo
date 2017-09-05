26516167f374d0f26a3b4c3756e9e06f7953c20b
Patrícia Duarte <patricia.duarte.dds@gmail.com>
2017-09-05 10:35:46 -0300

    Update morfologiaPassaro.py

--- morfologiaPassaro.py
+++ morfologiaPassaro.py
@@ -1,6 +1,8 @@
+#importa as bibliotecas que serao utilizadas
 import cv2
 import numpy as np
- 
+
+#Pega a imagem que sera processada
 imagem = cv2.imread('bird.jpg')
 
 # Aplica os respectivos filtros
@@ -14,11 +16,11 @@ bilateralFilter = cv2.bilateralFilter(imagem,9,75,75)
 # Escreve o nome de cada filtro na imagem correspondente
 font = cv2.FONT_HERSHEY_SIMPLEX
 cv2.putText(imagem,'Imagem original',(5,320), font, 1, (0,0,255), 2, cv2.LINE_AA)
-cv2.putText(filter2D,'Filter 2D',(45,320), font, 1, (0,0,255), 2, cv2.LINE_AA)
-cv2.putText(blur,'Blur',(80,320), font, 1, (0,0,255), 2, cv2.LINE_AA)
-cv2.putText(gaussianBlur,'Gaussian Blur',(10,320), font, 1, (0,0,255), 2, cv2.LINE_AA)
-cv2.putText(median,'Median',(60,320), font, 1, (0,0,255), 2, cv2.LINE_AA)
-cv2.putText(bilateralFilter,'Bilateral Filter',(10,320), font, 1, (0,0,255), 2, cv2.LINE_AA)
+cv2.putText(filter2D,'Filtro 2D',(45,320), font, 1, (0,0,255), 2, cv2.LINE_AA)
+cv2.putText(blur,'Borrao',(80,320), font, 1, (0,0,255), 2, cv2.LINE_AA)
+cv2.putText(gaussianBlur,'BorraoGaussiano',(10,320), font, 1, (0,0,255), 2, cv2.LINE_AA)
+cv2.putText(median,'Mediana',(60,320), font, 1, (0,0,255), 2, cv2.LINE_AA)
+cv2.putText(bilateralFilter,'Filtro Bilateral',(10,320), font, 1, (0,0,255), 2, cv2.LINE_AA)
 
 # Concatena as imagens para gerar a imagem de saida
 imagemTemporaria1 = np.concatenate((imagem, median, gaussianBlur), axis=1)
@@ -26,5 +28,5 @@ imagemTemporaria2 = np.concatenate((bilateralFilter, blur, filter2D), axis=1)
 imagemFinal = np.concatenate((imagemTemporaria1, imagemTemporaria2), axis=0)
 
 cv2.imwrite("bird1.png", imagemFinal)
-cv2.imshow("Final Image", imagemFinal)
+cv2.imshow("Imagem Final:", imagemFinal)
 cv2.waitKey(0)
