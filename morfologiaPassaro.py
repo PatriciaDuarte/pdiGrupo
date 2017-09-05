e77487dfad65e205638345c05a909a5956654e1c
Patrícia Duarte <patricia.duarte.dds@gmail.com>
2017-09-05 10:41:11 -0300

    Update morfologiaPassaro.py

--- morfologiaPassaro.py
+++ morfologiaPassaro.py
@@ -7,24 +7,24 @@ imagem = cv2.imread('bird.jpg')
 
 # Aplica os respectivos filtros
 kernel = np.ones((6,6),np.float32)/25
-filter2D = cv2.filter2D(imagem,-1,kernel)
-blur = cv2.blur(imagem,(5,5))
-gaussianBlur = cv2.GaussianBlur(imagem,(5,5),0)
-median = cv2.medianBlur(imagem,5)
-bilateralFilter = cv2.bilateralFilter(imagem,9,75,75)
+filtro2D = cv2.filter2D(imagem,-1,kernel)
+borrao = cv2.blur(imagem,(5,5))
+borraoGaussiano = cv2.GaussianBlur(imagem,(5,5),0)
+mediana = cv2.medianBlur(imagem,5)
+filtragemBilateral = cv2.bilateralFilter(imagem,9,75,75)
 
 # Escreve o nome de cada filtro na imagem correspondente
 font = cv2.FONT_HERSHEY_SIMPLEX
 cv2.putText(imagem,'Imagem original',(5,320), font, 1, (0,0,255), 2, cv2.LINE_AA)
-cv2.putText(filter2D,'Filtro 2D',(45,320), font, 1, (0,0,255), 2, cv2.LINE_AA)
-cv2.putText(blur,'Borrao',(80,320), font, 1, (0,0,255), 2, cv2.LINE_AA)
-cv2.putText(gaussianBlur,'BorraoGaussiano',(10,320), font, 1, (0,0,255), 2, cv2.LINE_AA)
-cv2.putText(median,'Mediana',(60,320), font, 1, (0,0,255), 2, cv2.LINE_AA)
-cv2.putText(bilateralFilter,'Filtro Bilateral',(10,320), font, 1, (0,0,255), 2, cv2.LINE_AA)
+cv2.putText(filtro2D,'Filtro 2D',(45,320), font, 1, (0,0,255), 2, cv2.LINE_AA)
+cv2.putText(borrao,'Borrao',(80,320), font, 1, (0,0,255), 2, cv2.LINE_AA)
+cv2.putText(borraoGaussiano,'BorraoGaussiano',(10,320), font, 1, (0,0,255), 2, cv2.LINE_AA)
+cv2.putText(mediana,'Mediana',(60,320), font, 1, (0,0,255), 2, cv2.LINE_AA)
+cv2.putText(filtragemBilateral,'Filtro Bilateral',(10,320), font, 1, (0,0,255), 2, cv2.LINE_AA)
 
 # Concatena as imagens para gerar a imagem de saida
-imagemTemporaria1 = np.concatenate((imagem, median, gaussianBlur), axis=1)
-imagemTemporaria2 = np.concatenate((bilateralFilter, blur, filter2D), axis=1)
+imagemTemporaria1 = np.concatenate((imagem, mediana, borraoGaussiano), axis=1)
+imagemTemporaria2 = np.concatenate((filtragemBilateral, borrao, filtro2D), axis=1)
 imagemFinal = np.concatenate((imagemTemporaria1, imagemTemporaria2), axis=0)
 
 cv2.imwrite("bird1.png", imagemFinal)
