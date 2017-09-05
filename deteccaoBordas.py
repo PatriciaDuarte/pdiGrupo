7f67ac5d9cc11123d922729b415167bc0a4d3a1f
Patrícia Duarte <patricia.duarte.dds@gmail.com>
2017-09-05 10:30:08 -0300

    Update deteccaoBordas.py

--- deteccaoBordas.py
+++ deteccaoBordas.py
@@ -1,11 +1,19 @@
+#importa as bibliotecas necessarias
 import numpy as np
 import cv2
 
+#Pega a imagem que sera processada
 imagem = cv2.imread('alanturing.jpg')
+#Converte uma imagem de um espaco de cores para outra
 imagem = cv2.cvtColor(imagem, cv2.COLOR_BGR2GRAY)
+#Faz a desfocagem gaussiana
 suave = cv2.GaussianBlur(imagem, (7, 7), 0)
+#Faz a deteccao de bordas simples
 canny1 = cv2.Canny(suave, 20, 120)
+#Faz a deteccao de bordas simples
 canny2 = cv2.Canny(suave, 70, 200)
+#Atribui para resultado  as duas imagens com bordas simples
 resultado = np.vstack([np.hstack([imagem,    suave ]),np.hstack([canny1, canny2])]) 
+#imprime na tela as imagens com bordas simples
 cv2.imshow("Detector de Bordas Canny", resultado)
 cv2.waitKey(0)
